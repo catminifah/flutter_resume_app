caab83975d16e80fd2f1b887bcb3ce871c8129cd
fah_tanaporn <daffsfe@gmail.com>
2025-05-30 10:41:53 +0700

    add

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -186,7 +186,6 @@ class _HomeScreen extends State<HomeScreen> {
                       //------------------------------------- Onboarding -------------------------------------//
                       OnboardingWidgetState(),
                       //------------------------------------- Onboarding -------------------------------------//
-                      
                     ],
                   ),
                 ),
@@ -206,9 +205,7 @@ class _HomeScreen extends State<HomeScreen> {
                           const SizedBox(height: 8),
                           const Divider(color: Colors.white24),
                           const SizedBox(height: 8),
-                          Expanded(
-                            child: buildResumeList(resumes),
-                          ),
+                          Expanded(child: buildResumeList(resumes),),
                         ],
                       ),
                     ),
@@ -239,10 +236,8 @@ class _HomeScreen extends State<HomeScreen> {
                         children: List.generate(icons.length, (index) {
                           final isSelected = _currentIndex == index;
                           final wasSelected = _previousIndex == index;
-                          final selectedColor =
-                              selectedColors[index % selectedColors.length];
-                          final selectediconColor = selectediconColors[
-                              index % selectediconColors.length];
+                          final selectedColor = selectedColors[index % selectedColors.length];
+                          final selectediconColor = selectediconColors[index % selectediconColors.length];
                           final Offset offset;
                           return GestureDetector(
                             key: _itemKeys[index],
@@ -611,73 +606,69 @@ class _HomeScreen extends State<HomeScreen> {
 
   //------------------------------ Widget Resume Header ----------------------------------------//
   Widget buildResumeList(List<Map<String, dynamic>> resumes) {
-    return Stack(
-      children: [
-        ListView.builder(
-          padding: const EdgeInsets.only(top: 16, bottom: 32),
-          shrinkWrap: true,
-          itemCount: resumes.length,
-          itemBuilder: (context, index) {
-            final item = resumes[index];
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-              child: Stack(
-                children: [
-                  Container(
+    return ListView.builder(
+      padding: const EdgeInsets.only(top: 16, bottom: 32),
+      shrinkWrap: true,
+      itemCount: resumes.length,
+      itemBuilder: (context, index) {
+        final item = resumes[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFF4E71FF).withOpacity(0.2),
+                      Color(0xFF8DD8FF).withOpacity(0.2),
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(16),
+                  border: Border.all(color: Colors.white24, width: 1),
+                  /*boxShadow: [
+                  BoxShadow(
+                    color: Colors.white24.withOpacity(0.1),
+                    blurRadius: 8,
+                    offset: const Offset(0, 4),
+                  ),
+                ],*/
+                ),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 2),
+                  leading: Container(
+                    width: 50,
+                    height: 50,
                     decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xFF4E71FF).withOpacity(0.2),
-                          Color(0xFF8DD8FF).withOpacity(0.2),
-                        ],
-                      ),
-                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.white10,
+                      shape: BoxShape.circle,
                       border: Border.all(color: Colors.white24, width: 1),
-                      /*boxShadow: [
-                      BoxShadow(
-                        color: Colors.white24.withOpacity(0.1),
-                        blurRadius: 8,
-                        offset: const Offset(0, 4),
-                      ),
-                    ],*/
                     ),
-                    child: ListTile(
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 2),
-                      leading: Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.white10,
-                          shape: BoxShape.circle,
-                          border: Border.all(color: Colors.white24, width: 1),
-                        ),
-                        child:
-                            const Icon(Icons.description, color: Colors.white),
-                      ),
-                      title: Text(
-                        item['title'] ?? 'Untitled',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Text(
-                        '${item['date']} | ${item['size']} | ${item['pages']}',
-                        style: const TextStyle(
-                          color: Colors.white70,
-                          fontSize: 12,
-                        ),
-                      ),
-                      trailing:
-                          const Icon(Icons.more_vert, color: Colors.white70),
+                    child:
+                        const Icon(Icons.description, color: Colors.white),
+                  ),
+                  title: Text(
+                    item['title'] ?? 'Untitled',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
+                  subtitle: Text(
+                    '${item['date']} | ${item['size']} | ${item['pages']}',
+                    style: const TextStyle(
+                      color: Colors.white70,
+                      fontSize: 12,
+                    ),
+                  ),
+                  trailing:
+                      const Icon(Icons.more_vert, color: Colors.white70),
+                ),
               ),
-            );
-          },
-        ),
-      ],
+            ],
+          ),
+        );
+      },
     );
   }
 
