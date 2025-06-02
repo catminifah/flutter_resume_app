6de9734dede117ea8c80da04ef74ac7c11e55710
fah_tanaporn <daffsfe@gmail.com>
2025-06-02 16:55:42 +0700

    add

--- lib/models/skill_category.dart
+++ lib/models/skill_category.dart
@@ -1,16 +1,20 @@
 class SkillCategory {
-  String category;
-  List<String> skills;
+  final String category;
+  final List<String> items;
 
-  SkillCategory({required this.category, required this.skills});
+  SkillCategory({
+    required this.category,
+    required this.items,
+  });
 
-  factory SkillCategory.fromJson(Map<String, dynamic> json) => SkillCategory(
-    category: json['category'],
-    skills: List<String>.from(json['skills']),
-  );
-
+  factory SkillCategory.fromJson(Map<String, dynamic> json) {
+    return SkillCategory(
+      category: json['category'] ?? '',
+      items: List<String>.from(json['items'] ?? []),
+    );
+  }
   Map<String, dynamic> toJson() => {
     'category': category,
-    'skills': skills,
+    'skills': items,
   };
 }
